6ed84be5842799714adc890f58600d7770b56376
rhit-BELLKM <bellkm@rose-hulman.edu>
2023-01-05 16:14:21 -0500

    init

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Boko Tutorial.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/overwatch play.mp4",
+    },
+];
